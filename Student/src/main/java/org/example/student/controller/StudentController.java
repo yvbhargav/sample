a0152f6a0bbfbc6c0b11d41f2990df7568224e1d
Bhargav <bhargav@email.com>
2025-10-07 12:51:06 +0530

    chnages done and project added to sample

---
+++ Student/src/main/java/org/example/student/controller/StudentController.java
@@ -0,0 +1,11 @@
+package org.example.student.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class StudentController {
+
+    @GetMapping("/get")
+    public String getStudent() {
+        return "Hello Student";
+    }
+}
