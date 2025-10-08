87d33f6e6af8d8cfa96a7598722262022d7a2b04
Bhargav <bhargav@email.com>
2025-10-08 08:50:30 +0530

    commits done  on 109

--- Student/src/main/java/org/example/student/controller/StudentController.java
+++ Student/src/main/java/org/example/student/controller/StudentController.java
@@ -6,7 +6,7 @@ public class StudentController {
 
     @GetMapping("/get")
     public String getStudent() {
-        String s="hi";
-        return "Hello Student+bhargav+main";
+
+        return "Hello 109";
     }
 }
